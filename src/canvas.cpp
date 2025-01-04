a96e6953e09069e01360ba4f93cc40d31e8d1ccb
puluboi <leo.juhoilmari@gmail.com>
2025-01-04 15:51:39 +0200

    added text

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -16,7 +16,6 @@ void meteor(uint8_t size, uint8_t speed, uint8_t start, uint8_t *canvas){
       for(int j = 0; j < size; j++){
         i-j - size >= 0 ? canvas[i-j-size] = 0 : canvas[OLED_WIDTH * OLED_HEIGHT] = 0; // remove the tail of the meteor
       }
-      OLED_clear(0);
       OLED_displayImg(canvas);
       delay(20);
     }
@@ -35,7 +34,7 @@ void drawSpinningSpiral(uint8_t *canvas) {
     int y = centerY + (int)(radius * sin(angle));
 
     if (x >= 0 && x < OLED_WIDTH && y >= 0 && y < OLED_HEIGHT) {
-      canvas[y * OLED_WIDTH + x] = 0xFF;
+      canvas[y * OLED_WIDTH + x] = 1; // Set 0xFF if u want full brightness, 1 if dim.
     }
 
     angle += angleIncrement;
@@ -71,6 +70,9 @@ void drawSpinningCube(uint8_t *canvas, unsigned int timeInMS) {
   int t = 0;
   while (t < timeInMS) {
     fillCanvas(0,canvas);
+    //paintString5x7("CUBE", 10, 10, canvas);
+    paintString3x5("California", 5, 20, canvas);
+    paintString3x5("dreamin'", 5, 30, canvas);
     for (int i = 0; i < 8; i++) {
       float x = (i & 1 ? 1 : -1) * size;
       float y = (i & 2 ? 1 : -1) * size;
@@ -143,4 +145,58 @@ void drawSpinningCube(uint8_t *canvas, unsigned int timeInMS) {
     t += 50;
     Serial.println(t);
   }
+}
+
+// Function to paint a character on the canvas using 5x7 font
+void paintChar5x7(char c, int x, int y, uint8_t *canvas) {    
+    // Get the font data for the character
+    const uint8_t* charData = font5x7[c - 32];
+
+    // Paint the character on the canvas
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (charData[i] & (1 << j)) {
+                canvas[(x + i) + (y + j) * 128] = 0xFF; // Set pixel to bright
+            } else {
+                canvas[(x + i) + (y + j) * 128] = 0; // Clear pixel
+            }
+        }
+    }
+}
+
+// Function to paint a character on the canvas using 3x5 font
+void paintChar3x5(char c, int x, int y, uint8_t *canvas) {    
+    // Get the font data for the character
+    const uint8_t* charData = font3x5[c - 32];
+
+    // Paint the character on the canvas
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (charData[i] & (1 << j)) {
+                canvas[(x + i) + (y + j) * 128] = 0xFF; // Set pixel to bright
+            } else {
+                canvas[(x + i) + (y + j) * 128] = 0; // Clear pixel
+            }
+        }
+    }
+}
+
+// Function to paint a string on the canvas using 5x7 font
+void paintString5x7(const char* str, int x, int y, uint8_t *canvas) {
+    int offsetX = 0;
+    while (*str) {
+        paintChar5x7(*str, x + offsetX, y, canvas);
+        offsetX += 6; // Move to the next character position (5 pixels for character + 1 pixel space)
+        str++;
+    }
+}
+
+// Function to paint a string on the canvas using 3x5 font
+void paintString3x5(const char* str, int x, int y, uint8_t *canvas) {
+    int offsetX = 0;
+    while (*str) {
+        paintChar3x5(*str, x + offsetX, y, canvas);
+        offsetX += 4; // Move to the next character position (3 pixels for character + 1 pixel space)
+        str++;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 uint8_t *canvas;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   InitSystem();
   OLED_init();
   uint16_t canvasSize = ((OLED_WIDTH%2==0)? (OLED_WIDTH/2): (OLED_WIDTH/2 + 1)) * OLED_HEIGHT;
@@ -21,17 +21,24 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("EERO");
   OLED_displayImg(gImage_eero);
   delay(1000);
-  OLED_clear(0);
+  paintString5x7("weather", 10, 10, canvas);
   delay(1000);
+  Serial.println("SPIRAL");
   drawSpinningSpiral(canvas);
   delay(1000);
+  fillCanvas(0, canvas);
   OLED_clear(0);
   delay(1000);
+  fillCanvas(0,canvas);
+  Serial.println("METEOR");
   meteor(6, 6, 48,canvas);
   
   fillCanvas(0,canvas);
+  Serial.println("CUBE");
+  
   drawSpinningCube(canvas, 20000);
   fillCanvas(0,canvas);
 }
